3eef597a831e37ac27c82d0cb24ccb1bcee47788
felipesarian <felipe.sarian@gmail.com>
2020-09-03 19:27:17 -0300

    adicionamos a logica do cadastro e o local storage

--- pages/cadastro/main.js
+++ pages/cadastro/main.js
@@ -1,26 +1,38 @@
 let users=[
     {username:'warlen', password:'123'}, {username:'felipe', password:'abc'}
 ]
+var lista = JSON.stringify(users)
+var lista2 = JSON.parse(lista)
 
 function onSubmit() {
   var username = document.getElementById("username").value;
   var password = document.getElementById("password").value;
+  var confirmpassword =  document.getElementById('confirm-password').value;
 
-  const loginInfo={
-    username:username,
-    password:password
-  }
-  const found = users.find((user) => loginInfo.username===user.username)
-  if(!found){
-      alert('Usuario nao encontrado')
+  const found = lista2.find((user) => username===user.username)
+  if(found){
+      alert('Usuario ja esta na base de dados')
+      return
   }
   else{
-      if(found.password===loginInfo.password){
-          window.location.href='pages/contador/index.html'
+      if(username === ''){
+          alert('voce precisa inserir um nome')
+          return
+      }
+      if(password.length >= 3){
+
+      if(password===confirmpassword){
+          localStorage.setItem('lastname', 'smith')
+          alert('voce foi cadastrado')
+          return
       }
       else{
-          alert('a senha esta errada')
+          alert('as senhas nao sao iguais')
+          return
       }
+    } else{
+       alert('sua senha precisa ter pelo menos 3 caracteres')
+       return
+    }
   }
-  console.log(found)
 }
